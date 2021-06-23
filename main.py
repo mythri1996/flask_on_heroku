1bd2b9942409a913d75b9a05eac7ae0adf5676cf
mythri1996 <mythrijain9@gmail.com>
2021-06-23 13:34:39 +0530

    first commit for our application

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return '<h1>this is    flask application <f1>'
+    return '<h1>this is flask application <f1>'
 
 if __name__=="__main__":
     app.run()
